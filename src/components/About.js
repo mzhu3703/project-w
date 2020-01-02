071fc785472ad212fb807a3af1a148796852236d
Michael <mzhu3703@gmail.com>
2020-01-01 19:57:16 -0500

    home and about page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from '@material-ui/core/Card';
+
+export default function About() {
+    const content =
+        <div>
+            <Card>yo mama</Card>
+            <br></br>
+            <Card>git</Card>
+        </div>
+    return content;
+}
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Home() {
+    return <h2>Home</h2>;
+  }
