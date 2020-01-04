d1f9f17fa22ef3b3014381aedf72632127ef0057
Michael <mzhu3703@gmail.com>
2020-01-04 15:27:44 -0500

    cards aligned side by side

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+
+    aboutCard: {
+        marginLeft: 0,
+        marginRight: 10,
+        flex: 1,
+    },
+
+    aboutTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+
+    git: {
+        marginRight: 3,
+        flex: 1,
+
+    },
+
+    body: {
+        wordWrap: 'break-word',
+    },
+
+    gitTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    }
+}));
 
 export default function About() {
+    const classes = useStyles();
     const content =
-        <div>
-            <Box>
-                <Card flexGrow={1}>yo mama</Card>
-            </Box>
-            <br></br>
-            <Box>
-                <Card>git</Card>
-            </Box>
+        <div style={{ display: 'flex', paddingTop: 20 }} >
+            <Card className={classes.aboutCard}>
+                <Typography className={classes.aboutTitle}>
+                    About Us
+                </Typography>
+                <Typography className={classes.body} component="p">
+                    djkaljdklsajjkdlsjaklfdjsklafdjsklajdklsjakldjsklajlkjddklsjakldjsalkjdkljslkjljklsajkdljsakdljaksljdlkasjkljsl!!!!
+                </Typography>
+            </Card>
+            <Card className={classes.git}>
+                <Typography className={classes.gitTitle}>
+                    Git
+                    </Typography>
+            </Card>
         </div>
     return content;
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,6 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     color: 'white',
-
   },
 }));
 
